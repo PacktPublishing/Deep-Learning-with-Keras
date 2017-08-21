59d50a9ad52881d72c5d9ad95f912f55377d0281
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-08-22 07:04:26 +0900

    Update Chapter04 notebook
    For cifar10 case

--- Chapter04/cifar10_utils.py
+++ Chapter04/cifar10_utils.py
@@ -1,3 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import unicode_literals
+import numpy as np
+from keras.datasets import cifar10
+
+
+def cifar10_process(x):
+    x = x.astype(np.float32) / 255.0
+    return x
+
+
+def cifar10_data():
+    (xtrain, ytrain), (xtest, ytest) = cifar10.load_data()
+    return cifar10_process(xtrain), cifar10_process(xtest)
