d330741454eafb6f6f6dd10083822610f508b735
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-09-25 06:06:17 +0900

    Update gan
    For keras 2.0 api

--- Chapter04/cifar10/example_gan_convolutional.py
+++ Chapter04/cifar10/example_gan_convolutional.py
@@ -3,19 +3,20 @@ import matplotlib as mpl
 # This line allows mpl to run with no DISPLAY defined
 mpl.use('Agg')
 
-from keras.layers import Flatten, Dropout, LeakyReLU, Input, Activation
+from keras.layers import Flatten, Dropout, LeakyReLU, Input, Activation, Dense, BatchNormalization
 from keras.models import Model
-from keras.layers.convolutional import UpSampling2D
+from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.optimizers import Adam
 from keras.datasets import mnist
 import pandas as pd
 import numpy as np
 import keras.backend as K
-from keras_adversarial.legacy import Dense, BatchNormalization, Convolution2D
 from keras_adversarial.image_grid_callback import ImageGridCallback
 from keras_adversarial import AdversarialModel, simple_gan, gan_targets
 from keras_adversarial import AdversarialOptimizerSimultaneous, normal_latent_sampling
 from image_utils import dim_ordering_fix, dim_ordering_input, dim_ordering_reshape, dim_ordering_unfix
+import os
+from keras.callbacks import TensorBoard
 
 
 def leaky_relu(x):
@@ -26,17 +27,17 @@ def model_generator():
     nch = 256
     g_input = Input(shape=[100])
     H = Dense(nch * 14 * 14)(g_input)
-    H = BatchNormalization(mode=2)(H)
+    H = BatchNormalization()(H)
     H = Activation('relu')(H)
     H = dim_ordering_reshape(nch, 14)(H)
     H = UpSampling2D(size=(2, 2))(H)
-    H = Convolution2D(int(nch / 2), 3, 3, border_mode='same')(H)
-    H = BatchNormalization(mode=2, axis=1)(H)
+    H = Conv2D(int(nch / 2), (3, 3), padding='same')(H)
+    H = BatchNormalization()(H)
     H = Activation('relu')(H)
-    H = Convolution2D(int(nch / 4), 3, 3, border_mode='same')(H)
-    H = BatchNormalization(mode=2, axis=1)(H)
+    H = Conv2D(int(nch / 4), (3, 3), padding='same')(H)
+    H = BatchNormalization()(H)
     H = Activation('relu')(H)
-    H = Convolution2D(1, 1, 1, border_mode='same')(H)
+    H = Conv2D(1, (1, 1), padding='same')(H)
     g_V = Activation('sigmoid')(H)
     return Model(g_input, g_V)
 
@@ -45,10 +46,18 @@ def model_discriminator(input_shape=(1, 28, 28), dropout_rate=0.5):
     d_input = dim_ordering_input(input_shape, name="input_x")
     nch = 512
     # nch = 128
-    H = Convolution2D(int(nch / 2), 5, 5, subsample=(2, 2), border_mode='same', activation='relu')(d_input)
+    H = Conv2D(int(nch / 2), (5, 5),
+               strides=(2, 2),
+               padding='same',
+               activation='relu',
+               )(d_input)
     H = LeakyReLU(0.2)(H)
     H = Dropout(dropout_rate)(H)
-    H = Convolution2D(nch, 5, 5, subsample=(2, 2), border_mode='same', activation='relu')(H)
+    H = Conv2D(nch, (5, 5),
+               strides=(2, 2),
+               padding='same',
+               activation='relu',
+               )(H)
     H = LeakyReLU(0.2)(H)
     H = Dropout(dropout_rate)(H)
     H = Flatten()(H)
@@ -107,13 +116,17 @@ if __name__ == "__main__":
     # train model
     generator_cb = ImageGridCallback("output/gan_convolutional/epoch-{:03d}.png",
                                      generator_sampler(latent_dim, generator))
+    callbacks = [generator_cb]
+    if K.backend() == "tensorflow":
+        callbacks.append(
+            TensorBoard(log_dir=os.path.join('output/gan_convolutional/', 'logs/'), histogram_freq=0, write_graph=True, write_images=True))
 
     xtrain, xtest = mnist_data()
     xtrain = dim_ordering_fix(xtrain.reshape((-1, 1, 28, 28)))
     xtest = dim_ordering_fix(xtest.reshape((-1, 1, 28, 28)))
     y = gan_targets(xtrain.shape[0])
     ytest = gan_targets(xtest.shape[0])
-    history = model.fit(x=xtrain, y=y, validation_data=(xtest, ytest), callbacks=[generator_cb], nb_epoch=100,
+    history = model.fit(x=xtrain, y=y, validation_data=(xtest, ytest), callbacks=[generator_cb], epochs=100,
                         batch_size=32)
     df = pd.DataFrame(history.history)
     df.to_csv("output/gan_convolutional/history.csv")
